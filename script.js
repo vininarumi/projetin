fd8ca7fd5144d8c5b95f3315f05f0db9ded6588e
Vinicius Narumi <seuemail@dominio.com>
2025-03-11 11:18:03 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode () {
+    const html = document.documentElement
+html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img")
+
+    if (html.classList.contains("light")) {
+        img.setAttribute('src','./assests/avatar-light.png')
+    
+    } else {
+
+    img.setAttribute('src','./assests/avatar.png')
+    }
+    
+}
+
+  
+
